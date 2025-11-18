8d14a96eab9a146b6f319f2166f56bdb4d71afd2
rajwaardiva28-debug <rajwaardiva28@gmail.com>
2025-11-18 14:56:41 +0700

    Update input.py

--- input.py
+++ input.py
@@ -1,1 +1,19 @@
+import streamlit as st
+
+st.title("Trayek Praktikum")
+
+st.write(
+    "Selamat Praktikum!"
+)
+
+input_NIM = st.text_input("Masukkan NIM: ", )
+pilihan_praktikum = ["LKD", "LFD"]
+
+pilihan_terpilih = st.selectbox(
+    "Praktikum yang akan dilakukan:",
+    pilihan_praktikum
+)
+
+st.button("Enter")
+
 
