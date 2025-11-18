3b58f836001c36219ed2eeaf624f53d740373cca
imadsabiq <imadsabiq21@gmail.com>
2025-11-18 14:11:10 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,9 @@ st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+st.write(
+    "ahsgdgafsd"
+)
+
+a = input("djasdbasd")
