72d3ad498a30b5c05444380017a60bf56585bbb0
rajwaardiva28-debug <rajwaardiva28@gmail.com>
2025-11-25 13:25:50 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,7 +28,7 @@ elif st.session_state.page == "page2":
     st.title("Halaman berikutnya")
 
     st.write(f':[NIM:]{input_NIM}')
-    st.write(f'[Praktikum dipilih:] {pilihan_terpilih}')
+    st.write(f':[Praktikum dipilih:] {pilihan_terpilih}')
 
     st.write("Ini halaman lanjutan setelah kamu tekan Enter.")
 
